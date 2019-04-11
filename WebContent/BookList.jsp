05e4cce66a97075f0b80fbc51fbdb404e03f1642
Julie Java <juliejava@icloud.com>
2019-04-11 17:33:33 -0500

    Completed up to routing pages

--- WebContent/BookList.jsp
+++ WebContent/BookList.jsp
@@ -1,15 +1,59 @@
 <?xml version="1.0" encoding="ISO-8859-1" ?>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 <title>Insert title here</title>
+<%-- added stylesheet reference but style sheet was not provided by Pluralsight so grabbed one used in a prior tutorial --%>
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/style.css" />
 </head>
 <body>
 
-<p>Book Title: + ${book_title}</p>
+<%-- 2090410 added single line for one book --%>
+<%-- <p>Book Title: + ${book_title}</p> --%>
+
+<%-- 2090411 added foreach loop for multiple books from an arraylist --%>
+<%-- 2090411 to do that, need to add Maven support to our project    --%>
+<%-- 2090411 so we can use the JSP standard tag library              --%>
+
+<%-- 
+<c:forEach items="${book_titles}" var="item">
+	<p>Book: ${item}</p>
+</c:forEach>
+ --%>
+
+<%-- 2090411 changed to accept an array of book objects              --%>
+<%--
+<c:forEach items="${book_list}" var="item">
+	<p>Book: ${item}</p>
+</c:forEach>
+ --%>
+
+<%-- 2090411 create column headings           --%>
+<div class = "container">
+	<h1>Book Store</h1>
+	<table>
+		<caption>List of Books</caption>
+		<tr>
+			<th>Title</th>
+			<th>Author</th>
+			<th>Price</th>
+		</tr>
+	</table>
+</div>
+
+<%-- 2090411 keep the foreach loop so we can create a row            --%>
+<c:forEach items="${book_list}" var="item">
+	<tr>
+		<td> ${item.getTitle()} </td>
+		<td> ${item.getAuthor()} </td>
+		<td> ${item.getPrice()} </td>
+	</tr>
+	<br> </br>
+</c:forEach>
 
 
 
--- src/com/pluralsight/ControllerServlet.java
+++ src/com/pluralsight/ControllerServlet.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,13 +25,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+ 
+	
+	/* 20190411 Add array list to print out list of books              */
+	//private ArrayList<String> bookTitles = new ArrayList<String>();
+	
+	/* 20190411 Changed array to Book class                           */
+	private ArrayList<Book> bookList = new ArrayList<Book>();
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public ControllerServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        bookList.add(new Book("To Kill a Mockingbird", "Harper Lee", 5.50f));
+        bookList.add(new Book("1984", "George Orwell", 4.50f));
+        bookList.add(new Book("Frankenstein", "Mary Shelley", 4.00f));
     }
 
 	/**
@@ -39,17 +49,22 @@ public class ControllerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	    // 20190329 added printwriter object, title
+		
+		//PrintWriter output = response.getWriter();
+		//output.println("hello controller servlet again and again");
+		
 		// 20190408 added BookForm.html and form reader
 		// 20190409 moved all the code to the doPost method
-
-//		request.setAttribute("book_title","1984");
-//		RequestDispatcher dispatcher = request.getRequestDispatcher("BookList.jsp");
-//		dispatcher.forward(request, response);
 		
+		// 20190410 added the dispatcher and the /BookList.jsp call
+		//request.setAttribute("book_title","1984 julie again and again and yet again");
 		
-		PrintWriter output = response.getWriter();
-		output.println("hello controller servlet again and again");
-		request.setAttribute("book_title","1984 julie again and again");
+		// 20190411 added the arraylist
+		//request.setAttribute("book_titles", bookTitles);
+		
+		// 20190411 change the arraylist to the Book class
+		request.setAttribute("book_list", bookList);
+				
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookList.jsp");
 		dispatcher.forward(request, response);
 	
